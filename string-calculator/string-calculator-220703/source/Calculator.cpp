69d55f4ec1c654bf0dc3275bec71cf93af2378ef
ormir <ormirgj@gmail.com>
2022-07-03 08:49:38 +0200

    220703 string calculator

---
+++ string-calculator/string-calculator-220703/source/Calculator.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Ormir Gjurgjej on 03.07.22.
+//
+
+#include <string>
+#include <sstream>
+#include "Calculator.h"
+
+using namespace std;
+
+int Calculator::add(string input) {
+    stringstream inputStream(input);
+    int number = 0;
+    int result = 0;
+    char delimiter = ',';
+    string exceptionMessage = "negatives not allowed:";
+    bool hasNegatives = false;
+
+    if (input.rfind("//", 0) == 0) {
+        inputStream.ignore(2);
+        inputStream >> delimiter;
+    }
+
+    while (inputStream >> number) {
+        result += number;
+
+        if (number < 0) {
+            hasNegatives = true;
+            exceptionMessage.append(" ");
+            exceptionMessage.append(to_string(number));
+        }
+
+        if (number > 1000)
+            result -= number;
+
+        if (inputStream.peek() == delimiter)
+            inputStream.ignore();
+    }
+
+    if (hasNegatives)
+        throw invalid_argument(exceptionMessage);
+
+    return result;
+}
---
+++ string-calculator/string-calculator-220703/source/Calculator.h
@@ -0,0 +1,16 @@
+//
+// Created by Ormir Gjurgjej on 03.07.22.
+//
+
+#ifndef STRING_CALCULATOR_220703_CALCULATOR_H
+#define STRING_CALCULATOR_220703_CALCULATOR_H
+
+
+class Calculator {
+
+public:
+    int add(std::string);
+};
+
+
+#endif //STRING_CALCULATOR_220703_CALCULATOR_H
